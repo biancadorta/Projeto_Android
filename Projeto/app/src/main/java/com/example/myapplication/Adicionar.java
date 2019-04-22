9bb73fb790b8e1612234c0e3ddb1a74b082f8ba4
Bianca Dorta <30930818+biancadorta@users.noreply.github.com>
2019-04-22 08:35:56 -0300

    update
    Mudei a classe Adicionar, acrescentando o intent, e a MainActivity colocar Bundle...

--- Projeto/app/src/main/java/com/example/myapplication/Adicionar.java
+++ Projeto/app/src/main/java/com/example/myapplication/Adicionar.java
@@ -1,13 +1,43 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Adicionar extends AppCompatActivity {
 
+    Button btnIncluir;
+    TextView txtEmail;
+    TextView txtNome;
+    TextView txtTelefone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar);
+
+        txtEmail = findViewById(R.id.txtEmail);
+        txtNome  = findViewById(R.id.txtNome);
+        txtTelefone = findViewById(R.id.txtPhone);
+        btnIncluir = findViewById(R.id.btnIncluir);
+
+        btnIncluir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Adicionar.this, MainActivity.class);
+                Bundle params = new Bundle(); //a classe Bundle é utilizada para definir o objeto que sera passado para a outra tela
+
+                //verificar se o usuario realmente preencheu os campos
+                params.putString("email", txtEmail.getText().toString());
+                params.putString("nome", txtNome.getText().toString());
+                params.putString("telefone", txtTelefone.getText().toString());
+
+                intent.putExtras(params); //o metodo putExtras e usado para receber o objeto Bundle que sera passado para a outra tela
+
+                startActivity(intent);
+            }
+        });
     }
 }
--- Projeto/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Projeto/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity{
 
     ListView listAlunos;
+    ArrayList<Aluno> listaAlunos = new ArrayList<>();
     FloatingActionButton btnAdicionar;
 
     @Override
@@ -19,19 +23,44 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        try{
+            Intent intent = getIntent();
+            Bundle params = intent.getExtras(); //o metodo getExtras -> retorna o objeto Bundle criado para enviar os parametros
+
+            String email = params.getString("email");
+            String nome  = params.getString("nome");
+            String fone  = params.getString("telefone");
+
+            Aluno aluno = new Aluno(nome,email,fone);
+            listaAlunos.add(aluno);
+        }
+        catch(Exception e){
+            Toast.makeText(getBaseContext(),"Ainda nao há alunos para exibição",Toast.LENGTH_SHORT).show();
+        }
+
+
         listAlunos = findViewById(R.id.listAlunos);
-        //btnAdicionar = findViewById(R.id.floatingActionButton);
-        ArrayList<Aluno> listaAlunos = new ArrayList<>();
+        btnAdicionar = findViewById(R.id.floatingActionButton);
+
+        btnAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         Aluno al1 = new Aluno("Bianca","biancadorta14@gmail.com", "19997280222");
-        Aluno al2 = new Aluno("Elisinhaa", "elisacs@hotmail.com","19998765435");
+        Aluno al2 = new Aluno("Elisinhaa","elisacs@hotmail.com","19998765435");
         listaAlunos.add(al1);
         listaAlunos.add(al2);
         listarAlunos(); //de inicio nao ha nenhum aluno noa array, so havera alunos quando houver a primeira
-                          //inclusao e as demais posteriores
+                        //inclusao e as demais posteriores
     }
 
     private void listarAlunos(){
         //Como vou pegar os alunos se é o começo?
         //pega os alunos do array list e os lista na tela
+        ArrayAdapter<Aluno> alunosAdapter = new ArrayAdapter<Aluno>(this,android.R.layout.simple_list_item_1,listaAlunos);
+        listAlunos.setAdapter(alunosAdapter);
     }
 }
