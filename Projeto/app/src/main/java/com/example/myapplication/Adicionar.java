e1648c4af6a3aa24e69d27fcd28f9e505c48e8b5
Bianca Dorta <30930818+biancadorta@users.noreply.github.com>
2019-04-30 11:40:09 -0300

    update mainActivity

--- Projeto/app/src/main/java/com/example/myapplication/Adicionar.java
+++ Projeto/app/src/main/java/com/example/myapplication/Adicionar.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Adicionar extends AppCompatActivity {
 
@@ -13,6 +17,8 @@ public class Adicionar extends AppCompatActivity {
     TextView txtEmail;
     TextView txtNome;
     TextView txtPhone;
+    TextView txtMsg;
+    ArrayList<Aluno> listaAlunos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,17 +28,30 @@ public class Adicionar extends AppCompatActivity {
         txtNome  = findViewById(R.id.txtNome);
         txtPhone = findViewById(R.id.txtPhone);
         btnIncluir = findViewById(R.id.btnIncluir);
+        txtMsg = findViewById(R.id.txtMsg);
 
         btnIncluir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intentAux = getIntent();
+                listaAlunos= (ArrayList<Aluno>)intentAux.getSerializableExtra("listaAlunos");
+
                 Intent intent = new Intent(Adicionar.this, MainActivity.class);
                 Bundle params = new Bundle(); //a classe Bundle é utilizada para definir o objeto que sera passado para a outra tela
 
-                //verificar se o usuario realmente preencheu os campos
+                if(txtEmail.equals("") || txtNome.equals("") || txtPhone.equals("")){
+                    txtMsg.setText("Preencha todos os campos.");
+                    txtNome.setText("");
+                    txtPhone.setText("");
+                    txtEmail.setText("");
+                    return;
+                }
+
                 params.putString("email", txtEmail.getText().toString());
                 params.putString("nome", txtNome.getText().toString());
                 params.putString("telefone", txtPhone.getText().toString());
+
+                intent.putExtra("listaAlunos",(Serializable) listaAlunos);
 
                 intent.putExtras(params); //o metodo putExtras e usado para receber o objeto Bundle que sera passado para a outra tela
 
--- Projeto/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Projeto/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,86 +10,64 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 import static android.R.layout.*;
 
 public class MainActivity extends AppCompatActivity{
 
-    ListView listAlunos;
-    ArrayList<Aluno> listaAlunos = new ArrayList<>();
+    ListView listViewAlunos;
     FloatingActionButton btnAdicionar;
+    ArrayList<Aluno> listaAluno;
+   // MinhaAsyncTask myAsync; //ele ira instanciar de novo
+    TextView txtMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try{
-            Intent intent = getIntent();
-            Bundle params = intent.getExtras(); //o metodo getExtras -> retorna o objeto Bundle criado para enviar os parametros
-
-            if(!intent.equals(null)){
-                String email = params.getString("email");
-                String nome = params.getString("nome");
-                String fone = params.getString("telefone");
-
-                Aluno aluno = new Aluno(nome, email, fone);
-               // if(myAsync.equals(null)) {
-                 //   myAsync = new MinhaAsyncTask();
-                MinhaAsyncTask myAsync = new MinhaAsyncTask();
-                myAsync.execute(aluno);
-                //}
-            }
-        }
-        catch(Exception e){
-            Toast.makeText(getBaseContext(),e.getMessage().toString(),Toast.LENGTH_SHORT).show();
-        }
-
-
-        listAlunos = findViewById(R.id.listAlunos);
         btnAdicionar = findViewById(R.id.btnAdicionar);
+        txtMsg = findViewById(R.id.txtMsg);
+        listaAluno = new ArrayList<Aluno>();
 
         btnAdicionar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent2 = new Intent(MainActivity.this, Adicionar.class);
+                intent2.putExtra("listaAlunos",(Serializable)listaAluno);
                 startActivity(intent2);
-                //Toast.makeText(getBaseContext(),"Entrou no click",Toast.LENGTH_SHORT).show();
             }
         });
-        //listarAlunos(); //de inicio nao ha nenhum aluno noa array, so havera alunos quando houver a primeira
-                        //inclusao e as demais posteriores*/
 
 
+        try{
+            Intent intent = getIntent();
+            Bundle params = intent.getExtras(); //o metodo getExtras -> retorna o objeto Bundle criado para enviar os parametros
+
+            if(!(intent==null)){
+                    String email = params.getString("email");
+                    String nome = params.getString("nome");
+                    String fone = params.getString("telefone");
+
+                    Aluno aluno = new Aluno(nome, email, fone);
+
+                    listaAluno = (ArrayList<Aluno>) intent.getSerializableExtra("listaAlunos");
+                    listaAluno.add(aluno);
+                    listarAlunos(listaAluno);
+            }
+        }
+        catch(Exception e){
+            Toast.makeText(getBaseContext(),"Ainda não há alunos para serem exibidos.",Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void listarAlunos(ArrayList<Aluno> lista){
-        ArrayAdapter<Aluno> alunosAdapter = new ArrayAdapter<Aluno>(this, simple_list_item_2,lista);
-        listAlunos.setAdapter(alunosAdapter);
+        ArrayAdapter<Aluno> alunosAdapter = new ArrayAdapter<Aluno>(this, simple_gallery_item,lista);
+        listViewAlunos.setAdapter(alunosAdapter);
     }
-
-    class MinhaAsyncTask extends AsyncTask<Aluno,Void, ArrayList<Aluno>> {
-        ArrayList<Aluno> listaAluno;
-
-        public MinhaAsyncTask() {
-            listaAlunos = new ArrayList<Aluno>();
-        }
-
-        @Override
-        protected ArrayList<Aluno> doInBackground(Aluno... alunos) {
-            Aluno al1 = alunos[0];
-            listaAlunos.add(al1);
-            return listaAlunos;
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<Aluno> alunos) {
-            listarAlunos(alunos);
-        }
-    }
-
-
 }
